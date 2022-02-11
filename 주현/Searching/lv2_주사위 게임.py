5b396a920b2612d46b0af0ad5961624e7b9dd31c
Joohyun <loove1997@naver.com>
2022-02-11 19:41:28 +0900

    Update lv2_주사위 게임.py

--- 주현/Searching/lv2_주사위 게임.py
+++ 주현/Searching/lv2_주사위 게임.py
@@ -1,16 +1,15 @@
 # https://school.programmers.co.kr/courses/13093/lessons/88780
 
+from itertools import product
+from collections import Counter
+
 def solution(monster, S1, S2, S3):
     total = S1 * S2 * S3
-    meet_mst = 0
     
-    for s1 in range(1, S1 + 1):
-        for s2 in range(1, S2 + 1):
-            for s3 in range(1, S3 + 1):
-                # meet monster
-                if 1 + s1 + s2 + s3 not in monster:
-                    meet_mst += 1
-    # 0%
-    if meet_mst == 0:
-        return 0
+    # 주사위 던져서 나올수 있는 모든 경우 sum값 기준으로 딕셔너리 생성
+    s_count = Counter(list(map(sum,product(range(1, S1 + 1), range(1, S2 + 1), range(1, S3 + 1)))))
     
+    # 몬스터 만나지 않는 경우의 수 
+    unmeet_mst = total - sum([s_count[m - 1] for m in monster])
+    
+    return int((unmeet_mst / total) * 1000)
